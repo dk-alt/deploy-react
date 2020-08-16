eb3b2182c768ee3b59f6a154bcf7014acc505c46
deepak kolare <kolare.deepak@gmail.com>
2020-08-17 00:30:54 +0530

    Updates

---
+++ precache-manifest.8c665bdab416ae0698e114c8ed5fc0b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc5a9f778d14523c48d1d989fa6241b",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "850d5a012a0bab596e5c",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "514645d7069882fd022b",
+    "url": "/deploy-react/static/js/2.eccc9e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.eccc9e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850d5a012a0bab596e5c",
+    "url": "/deploy-react/static/js/main.f58052f8.chunk.js"
+  },
+  {
+    "revision": "6ceaf7b559dcda959c55",
+    "url": "/deploy-react/static/js/runtime-main.d89e5f17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
